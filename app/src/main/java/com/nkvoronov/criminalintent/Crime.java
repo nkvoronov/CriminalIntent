48bfc5635310ae104a2c09f261e4b053ef71d951
Nikolay Voronov <nkvoronov@gmail.com>
2019-07-08 14:00:51 +0300

    Init

---
+++ app/src/main/java/com/nkvoronov/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.nkvoronov.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+}

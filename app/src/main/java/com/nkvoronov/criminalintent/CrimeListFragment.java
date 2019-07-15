7145a9db5b59239b1f433dba9af41a68ca0ec71f
Nikolay Voronov <nkvoronov@gmail.com>
2019-07-16 00:13:59 +0300

    update103

--- app/src/main/java/com/nkvoronov/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/nkvoronov/criminalintent/CrimeListFragment.java
@@ -28,6 +28,12 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();

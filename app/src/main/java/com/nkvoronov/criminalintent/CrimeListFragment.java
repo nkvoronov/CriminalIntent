b0490a1fd187c47c8369ed3374059d2c698d7e55
Nikolay Voronov <nkvoronov@gmail.com>
2019-07-15 13:52:28 +0300

    Update91

--- app/src/main/java/com/nkvoronov/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/nkvoronov/criminalintent/CrimeListFragment.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
